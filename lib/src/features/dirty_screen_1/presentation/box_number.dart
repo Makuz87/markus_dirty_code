5cc8c18935e12fa27ea3d5dd94612a976fcd702d
Markus Willmann <markuswillmann87@gmail.com>
2025-04-03 12:05:02 +0200

    Aufgabe 1

---
+++ lib/src/features/dirty_screen_1/presentation/box_number.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class boxNumber extends StatelessWidget {
+  final int number;
+
+  const boxNumber({
+    super.key,
+    required this.number,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12), color: Colors.redAccent),
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Icon(Icons.bug_report),
+          SizedBox(
+            width: 8,
+          ),
+          Text(
+            'Box $number',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
